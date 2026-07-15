050e7e5e2d4d93edc29ff4913d3b1ca29df53b58
PWEG <pwegaparat@gmail.com>
2026-07-15 02:17:14 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,3 @@
-const API_URL = "https://pweg-ai.pwegaparat.workers.dev";
-
-
-
-const input = document.getElementById("userInput");
-
-const sendBtn = document.getElementById("sendBtn");
-
-const messages = document.getElementById("messages");
-
-const typing = document.getElementById("typing");
-
-
-function addMessage(text,type){
-
-
-    const div=document.createElement("div");
-
-
-    div.className="message " + type;
-
-
-    div.innerText=text;
-
-
-    messages.appendChild(div);
-
-
-    messages.scrollTop =
-    messages.scrollHeight;
-
-}
-
 // ===========================
 // MENU
 // ===========================
@@ -95,7 +62,38 @@ document.querySelectorAll(".side-menu a").forEach(link=>{
 });
 
 
+const API_URL = "https://pweg-ai.pwegaparat.workers.dev";
 
+
+
+const input = document.getElementById("userInput");
+
+const sendBtn = document.getElementById("sendBtn");
+
+const messages = document.getElementById("messages");
+
+const typing = document.getElementById("typing");
+
+
+function addMessage(text,type){
+
+
+    const div=document.createElement("div");
+
+
+    div.className="message " + type;
+
+
+    div.innerText=text;
+
+
+    messages.appendChild(div);
+
+
+    messages.scrollTop =
+    messages.scrollHeight;
+
+}
 
 
 function showTyping(){
